4a9e1bcb5c679b7312bc944eebda3703f9b4abd4
ecorrin <158743367+ecorrin@users.noreply.github.com>
2024-02-04 18:24:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,20 +11,49 @@ state_unemployment = f"{url}/US_Unemployment_Oct2012.csv"
 state_data = pd.read_csv(open_url(state_unemployment))
 geo_json = json.loads(open_url(state_geo).read())
 
-m = folium.Map(location=[48, -102], zoom_start=3)
+m = folium.Map(location=[51.55, -0.0500], zoom_start=17)
 
-folium.Choropleth(
-    geo_data=geo_json,
-    name="choropleth",
-    data=state_data,
-    columns=["State", "Unemployment"],
-    key_on="feature.id",
-    fill_color="YlGn",
-    fill_opacity=0.7,
-    line_opacity=0.2,
-    legend_name="Unemployment Rate (%)",
-).add_to(m)
 
-folium.LayerControl().add_to(m)
+
+# Creating data set for Latitude, Longitude
+geolocations = [
+("51.53914", "-0.03511"),
+("51.53853", "-0.03616")
+]
+# data set created
+
+# marker creation - running through data set one by one to produce equivalent set of markers. The marker function seems to take coordinates as its parameters, cords.
+for cords in geolocations:
+    folium.Marker(location=[cords[0], cords[1]]).add_to(m)
+# end of marker creation
+
+# create new list of plant names that match the geolocations
+plant_names = [
+
+    "Wild chives",
+    "Magpie mushroom"
+]
+# end of list
+
+# add display info – note, for testing leave in the Lat and Long pop up display but strip out in final version
+for cords, name in zip(geolocations, plant_names):
+    folium.Marker(location=[cords[0], cords[1]],
+                  popup=f"Latitude:<br>{cords[0]}<br>"
+                        f"Longitude:<br>{cords[1]}<br>"
+                        f"Name:<br>{name}"
+                  ).add_to(m)
+# end display code
+
+# fit bounds function automatic zoom in
+south_west_corner = min(geolocations)
+north_east_corner = max(geolocations)
+my_map_name.fit_bounds([south_west_corner, north_east_corner])
+# end fit bounds code
+
+# save and run
+m.save("my_map_visual.html")
+m
+
+
 
 display(m, target="folium")
