8ee411e320ed58b2f2c20eef3844ac5a6002542c
ecorrin <158743367+ecorrin@users.noreply.github.com>
2024-02-07 19:24:17 +0000

    Update user_location.py

--- user_location.py
+++ user_location.py
@@ -1,4 +1,12 @@
+from pyscript import display
+import folium
+import json
+import pandas as pd
 import folium.plugins as plugins
+
+from pyodide.http import open_url
+
+
 
 m = folium.Map([41.97, 2.81])
 
